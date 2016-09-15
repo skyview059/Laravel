64810f7cb844bf6e1d95a3dcb0a77347e89f1f34
Hamid Abbasi <Hamid@Saqar-pc>
2016-09-15 23:51:17 +0430

    bower add

--- resources/views/front_end/master.blade.php
+++ resources/views/front_end/master.blade.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>LARA TEST</title>
         <meta name="keywords" content="Red Blog Theme, Free CSS Templates" />
         <meta name="description" content="Red Blog Theme - Free CSS Templates by templatemo.com" />
         <link href="{{URL::asset('front_end/css/templatemo_style.css')}}" rel="stylesheet" type="text/css" />
+        
+        <link href="{{asset('vendor\twitter\bootstrap\dist\css\bootstrap.css')}}" rel="stylesheet" type="text/css" />
 
     </head>
     <body>
