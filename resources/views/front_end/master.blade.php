e65966d9f8e92598b0168ff84c611b49808d1838
Hamid Abbasi <Hamid@Saqar-pc>
2016-09-16 04:01:14 +0430

    Version 1.1 Beta

--- resources/views/front_end/master.blade.php
+++ resources/views/front_end/master.blade.php
@@ -5,48 +5,59 @@
         <title>LARA TEST</title>
         <meta name="keywords" content="Red Blog Theme, Free CSS Templates" />
         <meta name="description" content="Red Blog Theme - Free CSS Templates by templatemo.com" />
-        <link href="{{URL::asset('front_end/css/templatemo_style.css')}}" rel="stylesheet" type="text/css" />
-        
-        <link href="{{asset('vendor\twitter\bootstrap\dist\css\bootstrap.css')}}" rel="stylesheet" type="text/css" />
+                
+        <script href="{{URL::asset('bower_components/jquery/dist/jquery.min.js')}}" ></script>
+        <link type="text/css" rel="stylesheet" href="{{URL::asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}" />
+        <link type="text/css" rel="stylesheet" href="{{URL::asset('bower_components/font-awesome/css/font-awesome.min.css')}}" />
 
+		<link href="{{URL::asset('front_end/css/templatemo_style.css')}}" rel="stylesheet" type="text/css" />
     </head>
     <body>
-
-        <div id="templatemo_top_wrapper">
-            <div id="templatemo_top">
-
-                <div id="templatemo_menu">
-
-                    <ul>
-                        <li><a href="{{URL::to('/')}}" class="current">Home</a></li>
-                       
-                       {{    $user_id=Session::get('user_id') }}
-                           @if($user_id !=NULL)
-                       
-                       <li><a href="{{URL::to('/user-logout')}}">Logout</a></li>
-                       @else
-                        <li><a href="{{URL::to('/sign-up')}}">Sign Up</a></li>
-                        <li><a href="{{URL::to('/sign-in')}}">Sign In</a></li>
-                        @endif
-                        
-                        <li><a href="{{URL::to('/contact')}}">Contact Us</a></li>
-                    </ul>    	
-
-                </div> <!-- end of templatemo_menu -->
-
-                <div id="twitter">
-                    <a href="#">follow us <br />
-                        on twitter</a>
-                </div>
-
+        <nav class="navbar navbar-inverse">
+        
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">
+                LARA TEST
+              </a>
             </div>
-        </div>
+            
+            {{  $user_id=Session::get('user_id') }}
+
+                                    
+            <ul class="nav navbar-nav"> 
+                <li class="active"><a href="{{URL::to('/')}}" class="current">Home</a></li>
+                
+                @if($user_id !=NULL)
+                	<li><a href="{{URL::to('/user-logout')}}">Logout</a></li>
+                @else
+                	<li><a href="{{URL::to('/sign-up')}}">Sign Up</a></li>
+                	<li><a href="{{URL::to('/sign-in')}}">Sign In</a></li>
+                @endif
+                        
+                <li><a href="{{URL::to('/contact')}}">Contact Us</a></li>
+          	</ul>
+            
+            <ul class="nav navbar-nav" style="float:right"> 
+            	<li>
+                	<a href="{{URL::to('/contact')}}" >
+                		<i class="fa fa-twitter" style="color:#00bbe1"></i> follow us on Twitter
+                    </a>
+                </li>
+            </ul>
+          
+         </div>
+       </nav>
+
+
+        
 
         <div id="templatemo_header_wrapper">
             <div id="templatemo_header">
 
                 <div id="site_title">
-                    <h1><a href="http://www.templatemo.com" target="_parent"><strong>Red Blog</strong><span>Free Blog Template in HTML CSS</span></a></h1>
+                    <h1><a href="http://www.templatemo.com" target="_parent"><strong>LARA TEST</strong>
+                    <span>Free Blog for more learning laravel</span></a></h1>
                 </div>
 
             </div>
@@ -70,7 +81,7 @@
                         @yield('friends')
 
                         <h4>Recent Blog</h4>
-                        <ul class="templatemo_list">
+                        <ul class="templatemo_list well">
                             <li><a href="http://www.templatemo.com" target="_parent">Free CSS Templates</a></li>
                             <li><a href="http://www.flashmo.com" target="_parent">Flash Templates</a></li>
                             <li><a href="http://www.templatemo.com/page/1" target="_parent">Free Blog Themes</a></li>
--- resources/views/pages/recent_blog.blade.php
+++ resources/views/pages/recent_blog.blade.php
@@ -1,7 +1,7 @@
 @extends('front_end.master')
 @section('recent_blog')
-<h4>Recent Blog</h4>
-<ul class="templatemo_list">
+<h4>Recent Blogs</h4>
+<ul class="templatemo_list well">
     @foreach($recent_blog as $v_blog)
     <li><a href="{{URL::to('/blog-details/'.$v_blog->id)}}">{{$v_blog->blog_title}}</a></li>
     @endforeach
